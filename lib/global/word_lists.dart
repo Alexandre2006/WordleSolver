e03d7104d89ac524bc6c70e0b05cfab57e9874d5
ThinkAlex <ahaddaddelaveau@icloud.com>
2022-05-13 08:37:17 -0700

    Compressed Text

--- lib/global/word_lists.dart
+++ lib/global/word_lists.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:wordle_solver/data/words5.dart';
@@ -7,11 +8,12 @@ Map<int, List<String>> wordLists = {};
 
 Future<void> initWords() async {
   for (int i = 2; i < 16; i++) {
-    final List<dynamic> dynamicWords =
-        jsonDecode(await rootBundle.loadString("lib/data/${i}letterWords.json"))
-            as List<dynamic>;
+    final String encodedJson = utf8.decode(gzip.decode(base64Decode(
+        await rootBundle.loadString("lib/data/${i}compressed.txt"))));
+    final List<dynamic> dynamicWords = jsonDecode(encodedJson) as List<dynamic>;
     wordLists[i] = dynamicWords.cast<String>();
   }
   wordLists[55] = List.from(wordLists[5]!);
   wordLists[5] = List.from(words5);
+  print(wordLists[2]);
 }
